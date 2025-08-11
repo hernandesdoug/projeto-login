f82d2558997c7aa778b256b0943fef573775431c
Douglas Hernandes <douglashernandes@icloud.com>
2025-08-11 16:14:56 -0300

    ajustando validacao login

--- js/signin.js
+++ js/signin.js
@@ -1,26 +1,26 @@
 // validar dados de login, fazer requisicoes da api, redirecionar pra pagina de usuario
-async function validateLogin(){
+
+async function validateLogin() {
     const email = document.getElementById("email").value;
+    
+    const response = await fetch(`http://localhost:3333/users/${email}`);
+    const data = await response.json();
+    console.log(data);
     const password = document.getElementById("password").value;
+    if(email === "" && password === "") {
+        alert("Please, inform an email and password to login!"); 
+    }else if (email === data.email && password === data.password) {
+        window.location.href = "signon.html";
+    } else if (email === data.email && password === "") {
+        alert("Password not informed, please, type it!");
+    } else if (email === data.email && password !== data.password) {
+        alert("Incorrect password!")
+    } else {
+        alert("User not found!");
+    };
 
-    const response = await fetch("http://localhost:3333/users");
-    const dataLogin = await response.json();
+};
 
-    console.log(dataLogin);
-
-    dataLogin.forEach((item) => {
-        if (email === "" && password === "") {
-            alert("Please, type a valid email and password!");
-        }else if (email === item.email && password === item.password) {
-            window.location.href = "signon.html";
-        }else if(email === item.email && password === ""){
-            alert("Password not informed, please, type it!");
-        }else if(email === item.email && password !== item.password){
-            alert("Incorrect password!")
-        }else {
-            alert("User not found!");
-        };
-    });  
-};    
-document.addEventListener("DOMContentLoaded", function() {
+   
+document.addEventListener("DOMContentLoaded", function () {
 });
--- js/signup.js
+++ js/signup.js
@@ -1,9 +1,31 @@
 // cadastrar os campos de usuario, validar campos, fazer requisicoes api.
-
-
-
-
-
-
-document.addEventListener("DOMContentLoaded", function() {
+async function formUser(event) {
+    event.preventDefault();
+    const fullName = document.getElementById("full-name").value;
+    const dateBirth = document.getElementById("date-birth").value;
+    const nationality = document.getElementById("select-nat").value;
+    const password = document.getElementById("password").value;
+    const email = document.getElementById("email").value;
+    const phoneNumber = document.getElementById("phone-number").value;
+    const documentType = document.getElementById("select-doc").value;
+    // const cfPassword = document.getElementById("cf-password").value;
+    const response = await fetch(`http://localhost:3333/users`, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json; charset=utf-8"
+        },
+        body: JSON.stringify({
+            fullName,
+            email,
+            dateBirth,
+            phoneNumber,
+            password,
+            nationality,
+            documentType})
+    });
+    console.log(response);
+    console.log(await response.json());
+    alert("User created successfully!");  
+};
+document.addEventListener("DOMContentLoaded", function () {
 });
